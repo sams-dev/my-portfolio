6027dc66faff9e33823cd43824bb7e795fb3c01d
sams-dev <samuel2905.sms@gmail.com>
2025-01-21 17:21:51 -0300

    Header component

---
+++ src/Components/Common/Header.tsx
@@ -0,0 +1,26 @@
+import Link from "next/link";
+import Image from "next/image";
+
+const Header = () => {
+    return (
+        <header>
+      <div className="bg-black text-white flex h-[90px] items-center justify-between">
+        <div className="flex items-center">
+        <Link href="/">
+          <Image src="/Sd.png" width={90} height={90} alt="Logo da Samsdev" />
+        </Link>
+        <span className="text-2xl">S A M S D E V </span>
+        </div>
+        
+        <nav className="flex gap-4">
+            <Link className="font-semibold" href="/">Sobre mim</Link>
+            <Link className="font-semibold" href="/projetos">Projetos</Link>
+            <Link className="font-semibold" href="/servicos">Serviços</Link>  
+            <Link className="font-semibold" href="/contatos">Entre em contato</Link>
+        </nav>
+      </div>
+    </header>
+    )
+};
+
+export default Header;
--- src/Components/Common/Layout.tsx
+++ src/Components/Common/Layout.tsx
@@ -1,6 +1,5 @@
-import Link from "next/link";
-import Image from "next/image";
 import { ReactNode } from "react";
+import Header from "./Header";
 
 interface LayoutProps {
     children: ReactNode;
@@ -8,23 +7,7 @@ interface LayoutProps {
 export const Layout = ({children}: LayoutProps) => {
   return (
     <>
-    <header>
-      <div className="bg-black text-white flex h-[90px] items-center justify-between">
-        <div className="flex items-center">
-        <Link href="/">
-          <Image src="/Sd.png" width={90} height={90} alt="Logo da Samsdev" />
-        </Link>
-        <span className="text-2xl">S A M S D E V </span>
-        </div>
-        
-        <nav className="flex gap-4">
-            <Link className="font-semibold" href="/">Sobre mim</Link>
-            <Link className="font-semibold" href="/projetos">Projetos</Link>
-            <Link className="font-semibold" href="/servicos">Serviços</Link>  
-            <Link className="font-semibold" href="/contatos">Entre em contato</Link>
-        </nav>
-      </div>
-    </header>
+    <Header/>
     {children}
     </>
     
