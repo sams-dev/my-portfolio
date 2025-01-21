e68008092b7a8a0a2d999803304146f95b815a92
sams-dev <samuel2905.sms@gmail.com>
2025-01-21 17:18:51 -0300

    estilização do Header

--- src/Components/Common/Layout.tsx
+++ src/Components/Common/Layout.tsx
@@ -10,15 +10,18 @@ export const Layout = ({children}: LayoutProps) => {
     <>
     <header>
       <div className="bg-black text-white flex h-[90px] items-center justify-between">
+        <div className="flex items-center">
         <Link href="/">
           <Image src="/Sd.png" width={90} height={90} alt="Logo da Samsdev" />
         </Link>
-        <span>SAMSDEV</span>
+        <span className="text-2xl">S A M S D E V </span>
+        </div>
+        
         <nav className="flex gap-4">
-            <Link href="/">Sobre mim</Link>
-            <Link href="/">Projetos</Link>
-            <Link href="/servicos">Serviços</Link>  
-            <Link href="/contatos">Entre em contato</Link>
+            <Link className="font-semibold" href="/">Sobre mim</Link>
+            <Link className="font-semibold" href="/projetos">Projetos</Link>
+            <Link className="font-semibold" href="/servicos">Serviços</Link>  
+            <Link className="font-semibold" href="/contatos">Entre em contato</Link>
         </nav>
       </div>
     </header>
