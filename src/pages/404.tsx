8fb74c565b0d2c9e388f18c9b052b627958304da
sams-dev <samuel2905.sms@gmail.com>
2025-01-20 22:56:58 -0300

    ajuste na organização dos elementos dentro da page404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -3,10 +3,12 @@ import Link from "next/link";
 const NotFound = () => {
   return (
     <div className="bg-red-600">
-      <h1>404</h1>
+      <h1> Page not found 
+        <span>404</span>
+      </h1>
       <p>Oops, não conseguimos encontrar essa página!</p>
+      <span>Clique no botão abaixo para ser redirecionado a página </span>
       <div>
-        <span>Clique no botão abaixo para ser redirecionado a página </span>
         <Link href="/">Ir para à Página Inicial</Link>
       </div>
     </div>
