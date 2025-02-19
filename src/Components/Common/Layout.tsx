4e8dfad9f0b04bb84604b9d19d493acad4e69b88
sams-dev <samuel2905.sms@gmail.com>
2025-02-19 16:03:24 -0300

    Atualização na Page Home, add de fonte padrão

--- src/Components/Common/Layout.tsx
+++ src/Components/Common/Layout.tsx
@@ -1,15 +1,22 @@
 import { ReactNode } from "react";
 import Header from "./Header";
+import { Nunito } from "next/font/google";
 
 interface LayoutProps {
     children: ReactNode;
 }
+const nunito = Nunito (
+  {
+    subsets: ['latin'],
+    weight: '400',
+  }
+);
 export const Layout = ({children}: LayoutProps) => {
   return (
-    <>
+    <div className={nunito.className}>
     <Header/>
     {children}
-    </>
+    </div>
     
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -24,7 +24,7 @@ const Home = () => {
       </Head>
       <main className="mx-[175] mt-[80] flex justify-center grid-cols-2">
         <div>
-          <b className={` ${roboto.className} text-4xl `}>
+          <b className={` ${roboto.className} text-4xl pl-2`}>
             Oi 👋🏽 SOU O SAMUEL DOS SANTOS
           </b>
           <h1 className="text-9xl">
@@ -33,7 +33,9 @@ const Home = () => {
             </span>{" "}
             <br /> <span className={` ${ubunto.className} `}>DEVELOPER</span>
           </h1>
-          <p className={` ${nunito.className} text-base text-slate-800 pt-2`}>
+          <p
+            className="text-base text-slate-800 pt-2 pl-2"
+          >
             Tenho 22 anos e sou desenvolvedor Frontend, atualmente atuando como
             freelancer. Especializo-me em criar sistemas que aprimoram a
             eficiência e produtividade dos meus clientes. Foco em oferecer
@@ -42,25 +44,43 @@ const Home = () => {
             de uso superior que atenda às necessidades específicas de cada
             cliente.
           </p>
-          <ul className="flex justufy-center gap-[30] mt-[40]">
-            <li className="flex justify-center bg-white w-[80] h-[80] rounded-[25] shadow-md">
-              <Image src="/html.svg" alt="" width={60} height={60} />{" "}
-            </li>
-            <li className="flex justify-center bg-white w-[80] h-[80] rounded-[25] shadow-md">
-              <Image src="/css.svg" alt="" width={60} height={60} />{" "}
-            </li>
-            <li className="flex justify-center bg-white w-[80] h-[80] rounded-[25] shadow-md">
-              <Image src="/js.svg" alt="" width={60} height={60} />{" "}
-            </li>
-            <li className="flex justify-center bg-white w-[80] h-[80] rounded-[25] shadow-md">
-              <Image src="/react.svg" alt="" width={60} height={60} />{" "}
-            </li>
-            <li className="flex justify-center bg-white w-[80] h-[80] rounded-[25] shadow-md">
-              <Image src="/nextjs.svg" alt="" width={60} height={60} />{" "}
-            </li>
-            <li className="flex justify-center bg-white w-[80] h-[80] rounded-[25] shadow-md">
-              <Image src="/twcss.svg" alt="" width={60} height={60} />{" "}
-            </li>
+          <ul className="flex justify-center gap-[30px] mt-[40px]">
+            <div>
+              <li className="flex items-center justify-center bg-white w-[80px] h-[80px] rounded-[25] shadow-md">
+                <Image src="/html.svg" alt="" width={60} height={60} />
+              </li>
+              <h2 className="text-center text-sm text-gray-400 mt-2 ">HTML 5</h2>
+            </div>
+            <div>
+              <li className="flex items-center justify-center bg-white w-[80px] h-[80px] rounded-[25] shadow-md">
+                <Image src="/css.svg" alt="" width={60} height={60} />
+              </li>
+              <h2 className="text-center text-sm text-gray-400 mt-2 ">CSS 3</h2>
+            </div>
+            <div>
+              <li className="flex items-center justify-center bg-white w-[80px] h-[80px] rounded-[25] shadow-md">
+                <Image src="/js.svg" alt="" width={60} height={60} />
+              </li>
+              <h2 className="text-center text-sm text-gray-400 mt-2 ">JavaScript</h2>
+            </div>
+            <div>
+              <li className="flex items-center justify-center bg-white w-[80px] h-[80px] rounded-[25] shadow-md">
+                <Image src="/react.svg" alt="" width={60} height={60} />
+              </li>
+              <h2 className="text-center text-sm text-gray-400 mt-2 ">React JS</h2>
+            </div>
+            <div>
+              <li className="flex items-center justify-center bg-white w-[80px] h-[80px] rounded-[25] shadow-md">
+                <Image src="/nextjs.svg" alt="" width={60} height={60} />
+              </li>
+              <h2 className="text-center text-sm text-gray-400 mt-2 ">Next JS</h2>
+            </div>
+            <div>
+              <li className="flex items-center justify-center bg-white w-[80px] h-[80px] rounded-[25] shadow-md">
+                <Image src="/twcss.svg" alt="" width={60} height={60} />
+              </li>
+              <h2 className="text-center text-sm text-gray-400 mt-2 ">Tailwind CSS</h2>
+            </div>
           </ul>
         </div>
         <div className="ml-[120] ">
@@ -96,9 +116,17 @@ const Home = () => {
           </button>
         </div>
       </main>
-      <div className={`${nunito.className} flex justify-center items-center gap-2 mt-20 text-[#0F0E14] text-opacity-70 text-base`}>
+      <div
+        className="flex justify-center items-center gap-2 mt-20 text-[#0F0E14] text-opacity-70 text-base"
+      >
         Acesse meus projetos
-        <Image className="opacity-70 rotate-90 arrow-float" src="/arrowdd.png" alt="" width={12} height={12} />
+        <Image
+          className="opacity-70 rotate-90 arrow-float"
+          src="/arrowdd.png"
+          alt=""
+          width={12}
+          height={12}
+        />
       </div>
     </>
   );
