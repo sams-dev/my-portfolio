fd4faeaa19bdc9183cdb1cfcb7f5b9d052d98472
sams-dev <samuel2905.sms@gmail.com>
2025-07-17 06:01:56 -0300

    ajuste no form e estilização

--- src/pages/contatos.tsx
+++ src/pages/contatos.tsx
@@ -8,35 +8,76 @@ const Contatos = () => {
       <Head>
         <title>Contatos | samsdev</title>
       </Head>
-      <section className="bg-red-500 mx-[175] mt-[40] h-[800]">
-        <h1 className="text-black text-center font-medium text-5xl">
-          Contatos
+      <section className="mx-[175] mt-[40] px-12 h-[800] bg-white rounded-[15px] shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]">
+        <h1 className="text-black text-center font-medium text-5xl pt-12 pb-[75px]">
+          CONTATOS
         </h1>
-        <div className="bg-purple-400 grid grid-cols-2">
+        <div className="grid grid-cols-2">
           <form
-            className="flex flex-col gap-2 w-[478] h-[448]"
+            className="flex flex-col px-12 py-9 gap-4 w-[478px] h-[390px] bg-[] text-white border-2 border-[#498EF3] rounded-xl"
             action="https://www.notion.so/samsdevtech/dev-3f3aa5c888d14622a698a909d66e6f5a"
             method="POST"
           >
-            <div className="flex gap-4">
+            <div className="flex gap-12 justify-center items-center">
               <div className="flex flex-col gap-2">
-                <label htmlFor="nome">Nome*</label>
-                <input type="text" id="nome" name="nome" required />
+                <label className="text-2xl font-medium text-[#498EF3]" htmlFor="nome">
+                  Nome*
+                </label>
+                <input
+                  placeholder="seu nome"
+                  className="w-[182px] rounded bg-transparent border-b border-[#498EF3]"
+                  type="text"
+                  id="nome"
+                  name="nome"
+                  required
+                />
               </div>
               <div className="flex flex-col gap-2">
-                <label htmlFor="email">Email*</label>
-                <input type="email" id="email" name="email" required />
+                <label className="text-2xl font-medium text-[#498EF3]" htmlFor="email">
+                  Email*
+                </label>
+                <input
+                  placeholder="username@gmail.com"
+                  className="w-[182px] rounded bg-transparent border-b border-[#498EF3]"
+                  type="email"
+                  id="email"
+                  name="email"
+                  required
+                />
               </div>
             </div>
             <div className="flex flex-col gap-2">
-              <label htmlFor="assunto">Assunto*</label>
-              <input type="text" id="assunto" name="assunto" required />
+              <label className="text-2xl font-medium text-[#498EF3]" htmlFor="assunto">
+                assunto*
+              </label>
+              <input
+                placeholder="seu assunto..."
+                className="w-[400px] text-black self-center rounded bg-transparent border-b border-[#498EF3]"
+                type="text"
+                id="assunto"
+                name="assunto"
+                required
+              />
             </div>
             <div className="flex flex-col gap-2">
-              <label htmlFor="mensagem">Mensagem</label>
-              <textarea id="mensagem" name="mensagem"></textarea>
+              <label className="text-2xl font-medium text-[#498EF3]" htmlFor="mensagem">
+                mensagem
+              </label>
+              <textarea
+                placeholder="Digite sua mensagem..."
+                className="w-[400px] h-[180px] self-center text-black rounded-[5px] border border-[#498EF3]"
+                id="mensagem"
+                name="mensagem"
+              ></textarea>
             </div>
-            <input type="submit" value="Enviar" />
+            <div className="flex justify-center items-center">
+              <button
+                type="submit"
+                className="bg-[#498EF3] text-white px-6 py-2 rounded-lg font-semibold hover:bg-blue-600 transition flex items-center gap-2"
+              >
+                Enviar <span className="text-2xl font-bold"><Image src="/send.svg" alt="" width={16} height={16}/> </span>
+              </button>
+            </div>
           </form>
           <div className="bg-blue-400">
             <h2>Vamos criar um projeto juntos, contate-me</h2>
